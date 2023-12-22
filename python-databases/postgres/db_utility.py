1c06f000f11791228d5de74409940c16999997d2
Jeffrey Burmood <Jeffrey-swdev@proton.me>
2023-12-22 06:53:03 -0700

    initial setup config and methods

---
+++ python-databases/postgres/db_utility.py
@@ -0,0 +1,12 @@
+""" this file contains the various methods used to interface with the PostgreSQL DB """
+
+import psycopg
+
+def db_connection():
+    """ create a connection to the postgresql db and return it """
+    pass
+
+def db_write():
+    """ write the provided values to the indicated DB table """
+    pass
+
